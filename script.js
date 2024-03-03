ed97844caca147ed88c3ebd6177d257cc72e8a7c
Kristian Hernandez <hernandez.cardenas10@gmail.com>
2024-03-02 23:59:33 -0600

    Starting

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeName(){
+    let name = prompt("¿Cúal es tu nombre?");
+    document.getElementById("name").textContent = "Hola " + name;
+    document.title = "Saludos " + name;
+
+}
